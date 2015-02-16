003779a62f38404277c6ca81895b920a351a13c9
li-xiaoli <xiaoli@wcw-stud-145-109-4-142.wireless.uva.nl>
2015-02-16 12:45:44 +0100

    add reference

--- src/home.js
+++ src/home.js
@@ -1,7 +1,8 @@
 /*
  *  http://alignedleft.com/tutorials/d3/binding-data
  *  API https://github.com/mbostock/d3/wiki/API-Reference
- */
+ *  http://chimera.labs.oreilly.com/books/1230000000345/index.html
+ */ 
 
 ///////////  DOM  /////////// 
 
